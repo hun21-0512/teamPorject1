559e56e0e44355e51ee50965f759503a044d344c
user <user@DESKTOP-PR41A4C>
2022-11-04 22:04:55 +0900

    test

---
+++ test/src/test/MainA.java
@@ -0,0 +1,23 @@
+package test;
+
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.HeadlessException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+import javax.swing.WindowConstants;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+
+public class MainA extends JFrame {
+	public static void main(String[] args) {
+		System.out.println("221104 git test");
+	  }
+}
